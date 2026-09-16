78b52a0b546f406672692e1ab19f8c6826241c98
fidha-555 <106099766+fidha-555@users.noreply.github.com>
2026-09-16 11:01:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.61%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isMiddleElementUnique(self, nums: list[int]) -> bool:
+        middle = nums[len(nums) // 2]
+        if nums.count(middle) == 1:
+            return True
+        else:
+            return False
