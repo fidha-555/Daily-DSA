3990523ac0d1d17c75f5ee50294dc794e44d2a7a
fidha-555 <106099766+fidha-555@users.noreply.github.com>
2026-09-18 11:12:28 +0530

    Time: 63 ms (59.04%), Space: 19.4 MB (17.27%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        first = nums[:n]
+        second = nums[n:]
+        result = []
+        for i in range(n):
+            result += [first[i] , second[i]]
+        return result
